68d5bc7c9ac52af8eae09cd2eda62e5130161e9a
Norbert <kieler103@gmail.com>
2024-03-30 12:22:04 +0100

    eee

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q="
 CITY = input("Enter your city to see the weather!")
 url = BASE_URL + CITY + "&appid=" + API_KEY
 
-def unix_to_date(unix_timestamp):
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(unix_timestamp))
+
+def unix_to_date(unix_timestamp, unix_timezone):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(unix_timestamp + unix_timezone))
+
 
 try:
     response = requests.get(url).json()
@@ -20,15 +22,14 @@ try:
         humidity = response["main"]["humidity"]
         wind_speed = response["wind"]["speed"]
         country = response["sys"]["country"]
-        sunrise = response["sys"]["sunrise"] - response["timezone"]
-        sunset = response["sys"]["sunset"] - response["timezone"]
-        print(f"The Weather in {CITY} ({country}) is {weather_description}, sunrise at {unix_to_date(sunrise)[11:]}"
-              f" and sunset at {unix_to_date(sunset)[11:]}. \nThe temperature is {int(temperature)} but it feels like i"
+        timezone = response["timezone"]
+        sunrise = unix_to_date(response["sys"]["sunrise"], timezone)[11:]
+        sunset = unix_to_date(response["sys"]["sunset"], timezone)[11:]
+        print(f"The Weather in {CITY} ({country}) is {weather_description}, sunrise at {sunrise}"
+              f" and sunset at {sunset}. \nThe temperature is {int(temperature)} but it feels like i"
               f"t is {int(temperature_feels_like)}, humidity is at {humidity}% \nThe wind speed is {wind_speed} km/h"
               f", pressure is at {pressure} hPA")
     else:
         print("City not found!")
 except Exception as e:
-    print("an error occured:", e)
-
-
+    print("an error occurred:", e)
